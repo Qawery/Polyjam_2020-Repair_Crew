bb7f11c910efd585ead180d10f95dcf5aa0b5065
Wooki <wooki1@op.pl>
2020-01-31 22:33:22 +0100

    Unit with collision test

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+
+namespace Polyjam2020
+{
+	public class Unit : MonoBehaviour
+	{
+		private Node nodeUnderEffect = null;
+
+
+		public Node NodeUnderEffect => nodeUnderEffect;
+
+
+		private void Awake()
+		{
+			var trigger = GetComponent<Collider>();
+			Assert.IsNotNull(trigger, "Missing collider component on: " + gameObject.name);
+			Assert.IsTrue(trigger.isTrigger, "Collider is not trigger on: " + gameObject.name);
+		}
+
+		private void Update()
+		{
+			if (nodeUnderEffect != null)
+			{
+				//TODO: wywrzeć efekt na node
+			}
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			var otherNode = other.GetComponent<Node>();
+			if (otherNode != null)
+			{
+				Assert.IsNull(nodeUnderEffect, "NodeUnderEffect already present on: " + gameObject.name);
+				nodeUnderEffect = otherNode;
+			}
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			var otherNode = other.GetComponent<Node>();
+			if (otherNode != null)
+			{
+				Assert.IsTrue(nodeUnderEffect == otherNode, "Exiting trigger with node different than nodeUnderEffect on: " + gameObject.name);
+				nodeUnderEffect = null; ;
+			}
+		}
+	}
+}
---
+++ Assets/Tests/UnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+
+namespace Polyjam2020
+{
+	public class UnitTests : MonoBehaviour
+	{
+		private const string TEST_SCENE_NAME = "UnitTestScene";
+
+
+		[UnityTest]
+		public IEnumerator NodeDetectionOnTriggerEnter()
+		{
+			SceneManager.LoadScene(TEST_SCENE_NAME);
+			yield return null;
+			yield return null;
+			Assert.IsTrue(SceneManager.GetActiveScene().name == TEST_SCENE_NAME, "Test scene not loaded");
+			var node = Object.FindObjectOfType<Node>();
+			Assert.IsNotNull(node, "Missing node");
+			var unit = Object.FindObjectOfType<Unit>();
+			Assert.IsNotNull(unit, "Missing unit");
+			Assert.IsNull(unit.NodeUnderEffect, "Unit already has NodeUnderEffect");
+			unit.transform.position = node.transform.position;
+			yield return null;
+			yield return null;
+			Assert.IsNotNull(unit.NodeUnderEffect, "Unit didn't register NodeUnderEffect");
+		}
+	}
+}
