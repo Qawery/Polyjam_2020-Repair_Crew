cce4aedfb22bf5d7cfdb376b468856f8deb76e51
Rafał Tyl <rafal.tyl@silverbullet.pl>
2020-02-01 22:34:11 +0100

    Implement a simple status applier

---
+++ Assets/Scripts/StatusApplier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Polyjam2020
+{
+	public class StatusApplier : MonoBehaviour
+	{
+		[SerializeField] private float fireStartInterval = 5.0f;
+		private float fireStartCooldown;
+		private Graph graph;
+		
+		private void Awake()
+		{
+			graph = FindObjectOfType<Graph>();
+			fireStartCooldown = fireStartInterval;
+		}
+
+		void Update()
+		{
+			if (fireStartCooldown > 0.0f)
+			{
+				fireStartCooldown -= Time.deltaTime;
+			}
+			else
+			{
+				fireStartCooldown = fireStartInterval;
+				var aliveNodes = graph.Nodes.Where(t => t.GetComponent<FireStatus>() == null).ToList<Node>();
+				int nodeToDamageIndex = Random.Range(0, aliveNodes.Count);
+				aliveNodes[nodeToDamageIndex].AddStatus<FireStatus>();
+			}
+		}
+	}
+}
