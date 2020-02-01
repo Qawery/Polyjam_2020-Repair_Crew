8ebaaff5fce857949178925542a1fc8eccf4c260
Rafał Tyl <rafal.tyl@silverbullet.pl>
2020-02-01 18:01:01 +0100

    Create truck prefab, clean up Node prefab, start implementing edge mesh generation logic

---
+++ Assets/Scripts/Map/RoadSegment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Polyjam2020
+{
+	public class RoadSegment : MonoBehaviour
+	{
+		[SerializeField] private Transform start;
+		[SerializeField] private Transform end;
+
+		public Transform Start => start;
+		public Transform End => end;
+	}
+}
