757765252edc899f64df2cf6ae9ef2bd084fead3
Rafał Tyl <rafal.tyl@silverbullet.pl>
2020-02-01 09:03:25 +0100

    Implement node selection logic

--- Assets/Scripts/Input/NodeController.cs
+++ Assets/Scripts/Input/NodeController.cs
@@ -1,9 +1,101 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Polyjam2020
 {
 	public class NodeController : MonoBehaviour
 	{
+		[SerializeField] private LayerMask nodeLayers;
+		private List<Node> nodes = new List<Node>();
+		private Node selectedNode = null;
+
+		private Node SelectedNode
+		{
+			get => selectedNode;
+			set
+			{
+				if (selectedNode != null && selectedNode != value)
+				{
+					selectedNode.GetComponent<UnitSelectionComponent>().Deselect();
+				}
+				
+				selectedNode = value;
+				
+				if (selectedNode != value)
+				{
+					selectedNode.GetComponent<UnitSelectionComponent>().Select();
+				}
+				
+				OnSelectedNodeChanged?.Invoke(selectedNode);
+			}
+		}
+
+		public event System.Action<Node> OnSelectedNodeChanged;
 		
+		[SpawnHandlerMethod]
+		private void OnNodeSpawned(Node spawnedNode)
+		{
+			var selectionComponent = spawnedNode.GetComponent<NodeSelectionComponent>();
+			selectionComponent.OnSelected += OnNodeSelected;
+			selectionComponent.OnDeselected += OnNodeDeselected;
+			nodes.Add(spawnedNode);
+		}
+
+		[DestroyHandlerMethod]
+		private void OnSelectableUnitDestroyed(Node destroyedNode)
+		{
+			nodes.Remove(destroyedNode);
+		}
+
+		private void OnNodeSelected(Node node)
+		{
+			foreach (var registeredNode in nodes)
+			{
+				if (registeredNode != node)
+				{
+					registeredNode.GetComponent<UnitSelectionComponent>().Deselect();
+				}
+			}
+
+			selectedNode = node;
+		}
+
+		private void OnNodeDeselected(Node node)
+		{
+			if (node == SelectedNode)
+			{
+				selectedNode = null;
+			}
+		}
+		
+		private void Update()
+		{
+			ProcessNodeControls();
+		}
+
+		private void ProcessNodeControls()
+		{
+			if (Input.GetKeyUp(KeyCode.Mouse0))
+			{
+				var selectionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(selectionRay, out var hit, 100, nodeLayers))
+				{
+					var node = hit.collider.GetComponent<Node>();
+					Assert.IsNotNull(node,
+						$"There is no Node component on {hit.collider.gameObject.name} and yet it's on Node physics layer");
+
+					if (node == SelectedNode)
+					{
+						SelectedNode = null;
+					}
+					else
+					{
+						SelectedNode = node;
+					}
+				}
+			}
+		}
 	}
 }
