a8be09f6d01aec4e182d043f6a551a0cc53acbc7
Rafał Tyl <rafal.tyl@silverbullet.pl>
2020-02-02 16:33:14 +0100

    Click sounds

---
+++ Assets/Scripts/PlaySoundOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Polyjam2020
+{
+	public class PlaySoundOnClick : MonoBehaviour
+	{
+		[SerializeField] private AudioClip audioClip;
+		private void Awake()
+		{
+			GetComponent<Button>().onClick.AddListener(() =>
+			{
+				GetComponent<AudioSource>().PlayOneShot(audioClip);
+			});
+		}
+	}
+}
---
+++ Assets/Scripts/PlaySoundOnEnable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Polyjam2020
+{
+	public class PlaySoundOnEnable : MonoBehaviour
+	{
+		[SerializeField] private AudioClip audioClip;
+		private void OnEnable()
+		{
+			GetComponent<AudioSource>().PlayOneShot(audioClip);
+		}
+	}
+}
